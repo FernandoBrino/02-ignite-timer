8acfa1f748aa03f46eaeaaa3ee935c0e26209e97
Fernando Brino <fernandobrino11@gmail.com>
2022-08-21 23:28:42 -0300

    Integration TS with Styled Components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 
@@ -6,7 +6,26 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-style: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /* ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }} */
 `;
